5215b67ce5622fe099f090e210cd3d389423ac64
Mate Koszta <matekoszta@gmail.com>
2024-06-20 21:41:06 +0100

    tried to work out the indexing to extract kinematic variables

--- ID_backend.py
+++ ID_backend.py
@@ -1,6 +1,8 @@
 import numpy as np
 import numba as nb
 
+# np.random.seed(2)
+
 
 @nb.njit(fastmath=True, cache=True, parallel=True)
 def ID_backend(r_current, r_past, previos_deepest_ID, dt, c):
@@ -11,25 +13,48 @@ def ID_backend(r_current, r_past, previos_deepest_ID, dt, c):
     N_source = r_past.shape[0]
     N_obs = r_current.shape[0]
     N_t = r_past.shape[2]
-    IDs_array_positive = np.empty(shape=(N_source, N_obs), dtype=np.uint32)
-    IDs_array_negative = np.empty(shape=(N_source, N_obs), dtype=np.uint32)
-    denom_sign = np.empty(shape=(N_source, N_obs))
+    numerical_delta = 1e-16
+    IDs_array_positive = np.empty(shape=(N_obs, N_source), dtype=np.uint32)
+    IDs_array_negative = np.empty(shape=(N_obs, N_source), dtype=np.uint32)
+    interp_slope_sign = np.empty(shape=(N_obs, N_source))
 
-    prev_omega_sign = -1
-    for i in nb.prange(N_source):
-        for j in range(N_obs):
+    prev_omega = -np.inf
+    for i in nb.prange(N_obs):
+        for j in range(N_source):
             deepest_index = previos_deepest_ID[i, j]
+
             for t_idx in range(deepest_index, -1, -1):
 
-                spatial_sep = np.linalg.norm(r_current[j, :, 0]-r_past[i, :, t_idx])
+                spatial_sep = np.linalg.norm(r_current[i, :, 0]-r_past[j, :, t_idx])
 
                 temporal_sep = dt*c*t_idx
                 omega = spatial_sep - temporal_sep
 
-                if np.sign(omega) != prev_omega_sign:
+                if np.sign(omega) != np.sign(prev_omega):
                     IDs_array_negative[i, j] = t_idx + 1
                     IDs_array_positive[i, j] = t_idx
-                    break
-                prev_omega_sign = np.sign(omega)
+                    interp_slope_sign[i, j] = np.sign(np.abs(omega) - np.abs(prev_omega) + numerical_delta)
 
-    return IDs_array_negative, IDs_array_positive
+                    break
+                prev_omega = omega
+
+    return IDs_array_negative, IDs_array_positive, interp_slope_sign
+
+
+# c = 1
+# t = np.linspace(0, 5, 20)
+# x1 = 0.7 * t
+# x2 = -0.6 * t
+
+# pos_past = np.zeros((2, 3, 20))
+# pos_past[0, 0, :] = x1
+# pos_past[1, 0, :] = x2
+# pos_current = np.random.randn(30).reshape((10, 3, 1))
+
+# deepest_id = np.ones(dtype=np.uint32, shape=(10, 2)) * 19
+
+# dt = np.diff(t)[0]
+
+# U, V, D = ID_backend(pos_current, pos_past, deepest_id, dt, c)
+
+# print(U, V, D)
--- Kinematic_backend.py
+++ Kinematic_backend.py
@@ -1,7 +1,48 @@
-import numpy as np 
+import numpy as np
 
 
 import ID_backend
 
-def Extract_RetKinVar(r_tot, v_tot, a_tot):
-    return 
+
+def Extract_RetKinVar(r_tot, kinematic_var, ID_negative, ID_positive, ID_sign):
+    N_obs = ID_positive.shape[0]
+    N_source = ID_positive.shape[1]
+    N_T = kinematic_var.shape[-1]
+
+    x, y = np.meshgrid(range(3), range(N_source))
+
+    # print(x.shape, y.shape)
+    x = x[:, :, np.newaxis].repeat(N_obs, axis=-1)
+    y = y[:, :, np.newaxis].repeat(N_obs, axis=-1)
+
+    ID_positive = ID_positive.T[:, np.newaxis, :].repeat(3, axis=1)
+
+    # print(x.shape)
+    # print(y.shape)
+    # print(ID_positive.shape)
+
+    r_pos = r_tot[y, x, ID_positive]
+
+    return r_pos
+
+
+r_tot = kinematic_var = np.ones((10, 3, 100))
+ID_pos = np.ones((20, 10), dtype=np.uint32)
+# shape = (20, 10)
+# ID_pos = np.arange(np.prod(shape), dtype=np.uint32).reshape(shape)
+ID_neg = np.ones((20, 10), dtype=np.uint32)
+
+
+def Extract_RetKinVar_gpt(r_tot, kinematic_var, ID_negative, ID_positive, ID_sign):
+    N_obs = ID_positive.shape[0]
+    N_source = ID_positive.shape[1]
+    N_T = kinematic_var.shape[-1]
+
+    expanded_ID_pos = np.expand_dims(ID_positive, axis=0).repeat(N_source, axis=0)
+    print(expanded_ID_pos.shape)
+    r_pos = np.take_along_axis(r_tot, expanded_ID_pos, axis=2)
+
+    return r_pos
+
+
+print(Extract_RetKinVar(r_tot, kinematic_var, ID_neg, ID_pos, 0))
