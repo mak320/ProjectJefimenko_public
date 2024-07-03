587e91a3dd71c6b2b87154be224625eb0f182023
ArchHem <84734676+ArchHem@users.noreply.github.com>
2024-07-03 15:47:44 +0200

    Standardized shape of current functions is achieved

--- ID_backend.py
+++ ID_backend.py
@@ -7,12 +7,12 @@ import numba as nb
 @nb.njit(fastmath=True, cache=True, parallel=True)
 def ID_backend(r_current, r_past, previos_deepest_ID, dt, c, alpha= 0.0):
     """This function generates an array that serves to index the kinemtic variables"""
-    # rc == array shape - (3, N_obs, 1)
-    # rp == array shape - (3, N_source, N_past_time)
+    # rc == array shape - (N_obs, 3, 1) (c = current)
+    # rp == array shape - (N_source, 3, N_past_time) (p = past)
 
     N_source = r_past.shape[0]
     N_obs = r_current.shape[0]
-    N_t = r_past.shape[2]
+      
     numerical_delta = 1e-16
     IDs_array_positive = np.empty(shape=(N_obs, N_source), dtype=np.uint32)
     IDs_array_negative = np.empty(shape=(N_obs, N_source), dtype=np.uint32)
@@ -65,3 +65,31 @@ def ID_backend(r_current, r_past, previos_deepest_ID, dt, c, alpha= 0.0):
 # U, V, D = ID_backend(pos_current, pos_past, deepest_id, dt, c)
 
 # print(U, V, D)
+
+
+# test case of id backend 
+"""
+r_obs = np.array([0,0,0.0]).reshape((1,3,1))
+
+c = 1
+v0 = 0.7
+t = np.linspace(0,-10,1000)
+
+N_t = t.shape[0]
+
+dt = np.abs(np.diff(t)[0])
+
+x0 = 1
+r_past = np.zeros((2,3,N_t))
+r_past[0,0,:] = x0
+r_past[1,1,:] = v0*t+3.0
+
+deepest_index = (N_t - 1)*np.ones(dtype=np.uint32, shape=(1,2))
+
+
+U, V, D = ID_backend(r_obs,r_past,deepest_index,dt,c,0.0)
+
+print(U)
+print(V)
+print(D)
+"""
--- Kinematic_backend.py
+++ Kinematic_backend.py
@@ -1,79 +1,153 @@
 import numpy as np
-import ID_backend
+import numba as nb
+from ID_backend import ID_backend
+import matplotlib.pyplot as plt
+
+@nb.njit(parallel = True, cache = True)
+def lift_Indeces(smallest_index, kinematic_var, r_tot, ID_negative, ID_positive):
+    N_obs = ID_positive.shape[0]
+    N_source = ID_positive.shape[1]
+
+    smallest_retarded_quant = np.zeros((N_obs,N_source,3))
+    r_smallest = np.zeros((N_obs,N_source,3))
+
+    r_pos = np.zeros((N_obs,N_source,3))
+    r_neg = np.zeros((N_obs,N_source,3))
+
+    pos_retarded_quant = np.zeros((N_obs,N_source,3))
+    neg_retarded_quant = np.zeros((N_obs,N_source,3))
+
+
+    for i in nb.prange(N_obs):
+        for j in range(N_source):
+            
+            #shitty loop unrolling for the 3 cart directions 
+            lindex = smallest_index[i,j] # local index
+            smallest_retarded_quant[i,j,0] = kinematic_var[j,0,lindex]
+            smallest_retarded_quant[i,j,1] = kinematic_var[j,1,lindex]
+            smallest_retarded_quant[i,j,2] = kinematic_var[j,2,lindex]
+
+            r_smallest[i,j,0] = r_tot[j,0,lindex]
+            r_smallest[i,j,1] = r_tot[j,1,lindex]
+            r_smallest[i,j,2] = r_tot[j,2,lindex]
+
+            pindex = ID_positive[i,j] # positive index pairwise
+            r_pos[i,j,0] = r_tot[j,0,pindex]
+            r_pos[i,j,1] = r_tot[j,1,pindex]
+            r_pos[i,j,2] = r_tot[j,2,pindex]
+
+            pos_retarded_quant[i,j,0] = kinematic_var[j,0,pindex]
+            pos_retarded_quant[i,j,1] = kinematic_var[j,1,pindex]
+            pos_retarded_quant[i,j,2] = kinematic_var[j,2,pindex]
+
+            nindex = ID_negative[i,j] # negative index pairwise
+            r_neg[i,j,0] = r_tot[j,0,nindex]
+            r_neg[i,j,1] = r_tot[j,1,nindex]
+            r_neg[i,j,2] = r_tot[j,2,nindex]
+
+            neg_retarded_quant[i,j,0] = kinematic_var[j,0,nindex]
+            neg_retarded_quant[i,j,1] = kinematic_var[j,1,nindex]
+            neg_retarded_quant[i,j,2] = kinematic_var[j,2,nindex]
+
+    return smallest_retarded_quant, r_smallest, r_pos, r_neg, pos_retarded_quant, neg_retarded_quant
+
+
 
 def Extract_RetKinVar(r_tot, r_current, kinematic_var, ID_negative, ID_positive, ID_sign, dt, c, alpha=0.0):
 
-    """r_current is assumed to (3, N_obs, 1) shaped"""
+    """
+    r_tot has a shape of (N_source, 3, N_t)
+    r_current has a shape of (N_obs, 3, 1)
+    """
 
     N_obs = ID_positive.shape[0]
     N_source = ID_positive.shape[1]
     N_T = kinematic_var.shape[-1]
 
-    x, y = np.meshgrid(range(3), range(N_source))
-
-    
-    x = x[:, :, np.newaxis].repeat(N_obs, axis=-1)
-    y = y[:, :, np.newaxis].repeat(N_obs, axis=-1)
-
-    ID_positive = ID_positive.T[:, np.newaxis, :].repeat(3, axis=1)
-    ID_negative = ID_negative.T[:, np.newaxis, :].repeat(3, axis=1)
 
     """The resulting arrays of the retarded kinematic positions will have a shape of 
-    (N_source, 3, N_obs)"""
+    (N_obs, N_source, 3)"""
 
     """These represent along the axis as:
-    0th: ...Which source was at which positions
-    1th: ...Which component of the retarded quantity
-    2th: ...As seen by which observer"""
-
+    0th: ...As seen by which observer...
+    1th: ...Which source was at which positions...
+    2th: ...Which component of the retarded quantity"""
 
     """The ID sign array implicitly stores which of the two quanties of omega are closer to 0:
     If ID_sign[i,j] > 0 we infer that that ID_negative associated time moment is closer to the true retarded time moment
     IF ID_sign[i,j] < 0 we infer that that ID_positive associated time moment is closer to the true retarded time moment"""
 
-    ID_sign_expanded = ID_sign.T[:, np.newaxis, :]
-    
-    r_current_expanded = np.swapaxes(np.swapaxes(r_current,0,1),0,2)
-
     """Thus the time index of smallest difference is:"""
 
-    smallest_index = ((ID_sign_expanded+1)/2).astype(np.uint32) * ID_negative - ((ID_sign_expanded-1)/2).astype(np.uint32) * ID_positive
+    smallest_index = ((ID_sign+1)/2).astype(np.int32) * ID_negative - ((ID_sign-1)/2).astype(np.int32) * ID_positive
 
-    R_aux = r_current_expanded - r_tot[y,x,smallest_index]
+    smallest_retarded_quant, r_smallest, r_pos, r_neg, pos_retarded_quant, neg_retarded_quant = lift_Indeces(smallest_index, kinematic_var, r_tot, ID_negative, ID_positive) 
 
-    time_differential = (alpha - smallest_index[:,0,:])*dt
+    #(N_obs, N_source, 3)
+    r_current = np.swapaxes(r_current,1,2)
+    R_aux = r_current - r_smallest # ok
 
-    quasi_veloc = (r_tot[y,x,ID_positive] - r_tot[y,x,ID_positive]) / dt
+    #(N_obs, N_soruce)
+    time_differential = (alpha - smallest_index)*dt
+
+    #(N_obs, N_soource, 3)
+    quasi_veloc = (r_neg - r_pos) / dt
 
     """CHECK TIME SCALING"""
     optim = 'greedy'
 
-    R2 = np.einsum('ijk, ijk->ik', R_aux, R_aux, optimize=optim)
-
-    RW_b = np.einsum('ijk, ijk->ik', R_aux, quasi_veloc, optimize=optim)
+    #(N_obs, N_source)
+    R2 = np.einsum('ijk, ijk->ij', R_aux, R_aux, optimize=optim)
+    #(N_obs, N_source)
+    RW_b = np.einsum('ijk, ijk->ij', R_aux, quasi_veloc, optimize=optim)
 
     """tau minus t_k"""
+    # (N_obs, N_source)
     tau_diff_denom = 2*RW_b - c**2 * (time_differential)
     """uber numba incomaptible"""
+    #(N_obs, N_source)
     tau_diff = np.divide(R2 - c**2 * (time_differential)**2, tau_diff_denom, out=np.zeros_like(tau_diff_denom), where=tau_diff_denom!=0)
+    #(N_obs, N_source, 1)
+    tau_diff = tau_diff[:,:,np.newaxis]
 
-    tau_diff = tau_diff[:,np.newaxis,:]
-
-    retarded_quant_deriv = (kinematic_var[y,x,ID_positive] - kinematic_var[y,x,ID_negative]) / dt
-    improved_retarded_quant = kinematic_var[y,x,smallest_index] + retarded_quant_deriv * tau_diff
+    retarded_quant_deriv = (neg_retarded_quant - pos_retarded_quant) / dt
+    improved_retarded_quant = smallest_retarded_quant + retarded_quant_deriv * tau_diff
 
     return improved_retarded_quant
 
 
-r_tot = kinematic_var = np.ones((10, 3, 100))
-ID_pos = np.ones((20, 10), dtype=np.uint32)
+lis = []
+dts = np.linspace(0.00001,0.05,10)
+for i in range(len(dts)):
+    r_obs = np.array([0,0,0.0]).reshape((1,3,1))
 
-ID_neg = np.ones((20, 10), dtype=np.uint32)
+    c = 1
+    v0 = 0.5
 
-ID_sign = np.ones((20, 10), dtype=np.uint32)
+    t = np.arange(0.0,-10,-dts[i])
 
-Nobs = 20 
+    N_t = t.shape[0]
+    print(N_t)
 
-r_curr = np.random.randn(Nobs*3).reshape(3, Nobs, 1)
+    dt = np.abs(np.diff(t)[0])
 
-print(Extract_RetKinVar(r_tot, r_curr, kinematic_var, ID_neg, ID_pos, ID_sign, 1.0, 0.1).shape)
+
+    x0 = 1
+    r_past = np.zeros((2,3,N_t))
+    r_past[0,0,:] = x0
+    r_past[1,1,:] = v0*t+2.0
+    deepest_index = (N_t - 1)*np.ones(dtype=np.uint32, shape=(1,2))
+
+
+    U, V, D = ID_backend(r_obs,r_past,deepest_index,dt,c,0.0)
+    imprved_r = Extract_RetKinVar(r_past,r_obs,r_past,U,V,D,dt,c,0.0)
+    print(imprved_r.shape)
+    y = imprved_r[0,1,1]
+    lis.append(y)
+
+print(lis)
+lis = np.abs(np.asarray(lis)-4/3)
+
+plt.plot(dts, lis,"o")
+plt.show()
+#
